2125407e9f604789846042bf726534d568ec80c4
Eduardo Trindade <eduardoftrindade@yahoo.com>
2022-08-09 10:06:40 -0300

    Inclusão: Horário previsto

--- script.js
+++ script.js
@@ -12,8 +12,19 @@ function calculo() {
         "input[name='volume']:checked"
     ).value;
 
-    if (qtd_programada == "" || qtd_produzida == "") {
-        var result = "Por favor, digite as quantidades acima.";
+    if (qtd_programada == "") {
+        var result = "Por favor, digite a quantidade programada.";
+        document.getElementById("qtd-programada").focus();
+        return result;
+
+    } else if (qtd_produzida == "") {
+        result = "Por favor, digite a quantidade produzida."
+        document.getElementById("qtd-produzida").focus();
+        return result;
+    }
+
+    if (qtd_produzida > qtd_programada) {
+        result = "A quantidade produzida não pode ser maior do que a quantidade programada. Por favor, verificar.";
         document.getElementById("qtd-programada").focus();
         return result;
     }
@@ -21,15 +32,19 @@ function calculo() {
     if (volume_selecionado == "100") {
         var qtd_por_carro = 1904;
         var minutos_por_carro = 29;
+
     } else if (volume_selecionado == "250") {
         qtd_por_carro = 1372;
         minutos_por_carro = 38;
+
     } else if (volume_selecionado == "500") {
         qtd_por_carro = 936;
         minutos_por_carro = 19;
+
     } else if (volume_selecionado == "1000") {
         qtd_por_carro = 550;
         minutos_por_carro = 18;
+
     } else {
         alert("Você precisa escolher um volume!");
     }
@@ -38,21 +53,30 @@ function calculo() {
     var carros_restantes = qtd_restante / qtd_por_carro;
     var minutos_restantes = carros_restantes * minutos_por_carro;
 
+    var data = new Date();
+
+    data.setMinutes(data.getMinutes() + minutos_restantes)
+
+    var dataFormatada = data.toLocaleString("pt-br")
+
     if (minutos_restantes > 60) {
         var horas = (minutos_restantes / 60) | 0;
         var minutos = minutos_restantes % 60;
 
         result =
-            "Faltam, aproximadamente, " +
+            "Tempo restante: " +
             horas +
             " horas e " +
             Math.floor(minutos) +
-            " minutos para o fim da solução.";
+            " minutos." +
+            " Horário previsto: " + dataFormatada;
+
     } else {
         result =
-            "Faltam, aproximadamente, " +
+            "Tempo restante: " +
             Math.floor(minutos_restantes) +
-            " minutos para o fim da solução.";
+            " minutos." +
+            " Horário previsto: " + dataFormatada;
     }
 
     return result;
